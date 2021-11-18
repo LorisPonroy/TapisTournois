2cd4ce82515f93d14f85691dd194c78393841696
flave <flavien.robret@lilo.org>
2021-11-18 21:43:12 +0100

    réorga + ajout du back

---
+++ back/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send("le back fonctionne");
+    console.log("someone on /")
+});
+
+app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
