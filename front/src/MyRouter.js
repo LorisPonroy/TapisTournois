87f419afb1079773ce13878477a110baae092337
ValentinROUXEL <vrouxellycee@gmail.com>
2021-11-19 11:08:50 +0100

    ajout router

---
+++ front/src/MyRouter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+function MyRouter() {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<App />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default MyRouter
